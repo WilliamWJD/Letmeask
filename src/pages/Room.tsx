955609515de7f7289b776264c9749d9d14295d4a
williamwjd <william.jose.wjd@gmail.com>
2021-06-27 10:29:24 -0300

    criando a estrutura html da pagina room

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,34 @@
+import { Button } from '../components/Button';
+
+import logoImg from '../assets/images/logo.svg';
+
 export function Room(){
     return(
-        <h1>Sala</h1>
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <div>Código</div>
+                </div>
+            </header>
+
+            <main className="content">
+                <div className="romm-title">
+                    <h1>Sala React</h1>
+                    <span>4 perguntas</span>
+                </div>
+
+                <form>
+                    <textarea 
+                        placeholder="O que você quer perguntar ?"
+                    />
+
+                    <div className="form-footer">
+                        <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+                        <Button type="submit">Enviar pergunta</Button>
+                    </div>
+                </form>
+            </main>
+        </div>
     )
 }
