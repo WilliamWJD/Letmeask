9f47d650619d22dcafce1defb779dad87cc692e9
williamwjd <william.jose.wjd@gmail.com>
2021-06-30 20:47:29 -0300

    criando componente RoomCode

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,16 +1,28 @@
+import { useParams } from 'react-router-dom';
+
 import { Button } from '../components/Button';
 
 import logoImg from '../assets/images/logo.svg';
 
+import { RoomCode } from '../components/RoomCode';
+
 import '../styles/room.scss';
 
+type RoomParamsProps = {
+    id:string;
+}
+
 export function Room(){
+    const params = useParams() as RoomParamsProps;
+    
     return(
         <div id="page-room">
             <header>
                 <div className="content">
                     <img src={logoImg} alt="Letmeask" />
-                    <div>Código</div>
+                    <RoomCode
+                        code={params.id}
+                    />
                 </div>
             </header>
 
