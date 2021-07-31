26fb50894b32f76f55221235e82f33882cdcb620
williamwjd <william.jose.wjd@gmail.com>
2021-07-31 13:04:51 -0300

    cadastrando perguntas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import 'react-toastify/dist/ReactToastify.css';
+
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from './pages/Room';
 
 import { AuthContextProvider } from './contexts/AuthContextProvider';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <BrowserRouter>
+      <ToastContainer/>
       <AuthContextProvider>
         <Switch>
           <Route path="/" exact component={Home} />
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,47 +1,141 @@
+import { FormEvent, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { Button } from '../components/Button';
+import { RoomCode } from '../components/RoomCode';
 
 import logoImg from '../assets/images/logo.svg';
 
-import { RoomCode } from '../components/RoomCode';
-
 import '../styles/room.scss';
+import { useAuth } from '../hooks/useAuth';
+import { toast } from 'react-toastify';
+import { database } from '../services/firebase';
 
-type RoomParamsProps = {
+type FirebaseQuestions = Record<string, {
+    author:{
+        name:string;
+        avatar:string;
+    }
+    content:string;
+    isAnswered:boolean;
+    isHighlighted:boolean;
+}>
+
+type Questions = {
     id:string;
+    author:{
+        name:string;
+        avatar:string;
+    }
+    content:string;
+    isAnswered:boolean;
+    isHighlighted:boolean;
 }
 
-export function Room(){
+type RoomParamsProps = {
+    id: string;
+}
+
+export function Room() {
+    const { user } = useAuth();
     const params = useParams() as RoomParamsProps;
-    
-    return(
+    const [newQuestion, setNewQuestion] = useState('');
+    const [questions, setQuestions] = useState<Questions[]>([]);
+    const [title, setTitle] = useState('');
+
+    const roomId = params.id;
+
+    useEffect(() =>{
+        const roomRef = database.ref(`rooms/${roomId}`);
+
+        roomRef.on('value', room=>{
+            const databaseRoom = room.val();
+            const firebaseQuestions:FirebaseQuestions = databaseRoom.questions ?? {};
+
+            const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value])=>{
+                return{
+                    id:key,
+                    content:value.content,
+                    author:value.author,
+                    isHighlighted:value.isHighlighted,
+                    isAnswered:value.isAnswered
+                }
+            })
+
+            setTitle(databaseRoom.title)
+            setQuestions(parsedQuestions)
+        })
+
+
+    },[roomId])
+
+    async function handleSendQuestion(e: FormEvent) {
+        e.preventDefault();
+
+        if (newQuestion.trim() === '') {
+            return;
+        }
+
+        if (!user) {
+            toast.error("Nenhum usuário autenticado")
+            return;
+        }
+
+        const question = {
+            content: newQuestion,
+            author: {
+                name: user.name,
+                avatar: user.avatar,
+            },
+            isHighlighted: false,
+            isAnswered: false
+        }
+
+        await database.ref(`rooms/${roomId}/questions`).push(question);
+
+        toast.success("Pergunta enviada com sucesso!")
+
+        setNewQuestion("");
+    }
+
+    return (
         <div id="page-room">
             <header>
                 <div className="content">
                     <img src={logoImg} alt="Letmeask" />
                     <RoomCode
-                        code={params.id}
+                        code={roomId}
                     />
                 </div>
             </header>
 
             <main>
                 <div className="room-title">
-                    <h1>Sala React</h1>
-                    <span>4 perguntas</span>
+                    <h1>Sala {title}</h1>
+                    {questions.length > 0 && <span>{questions.length} perguntas</span>}
                 </div>
 
-                <form>
-                    <textarea 
+                <form onSubmit={handleSendQuestion}>
+                    <textarea
                         placeholder="O que você quer perguntar ?"
+                        onChange={(e) => setNewQuestion(e.target.value)}
+                        value={newQuestion}
                     />
 
                     <div className="form-footer">
-                        <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
-                        <Button type="submit">Enviar pergunta</Button>
+                        {user ? (
+                            <div className="user-info">
+                                <img src={user.avatar} alt={user.name} />
+                                <span>{user.name}</span>
+                            </div>
+                        ) : (
+                            <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+                        )}
+                        <Button type="submit" disabled={!user}>Enviar pergunta</Button>
                     </div>
                 </form>
+
+                {JSON.stringify(questions)}
             </main>
         </div>
     )
